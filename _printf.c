7abe114e02be4a7e0ebea50bb9ece44537292595
Yonatan <yonatanaddis0257@gmail.com>
2022-08-07 06:53:33 -0700

    Adding function that checks format specifiers

---
+++ _printf.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _printf - function to send formatted output to the screen.
+ * @format: pointer to the string tobe formated and printed.
+ * Return: number of characters printed.
+ */
+
+int	_printf(const char *format, ...)
+{
+	va_list	arg;
+	int i;
+	int c;
+
+	i = 0;
+	c = 0;
+	va_start(arg, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '\0')
+				return (0);
+			else if (format[i] == '%')
+				i = _putchar(format[i], i, &c);
+			else
+				i = check_formater(format[i], &arg, i, &c);
+		}
+		else
+			i = _putchar(format[i], i, &c);
+	}
+	va_end(arg);
+
+	return (c - 1);
+}
