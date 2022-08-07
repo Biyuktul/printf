7507ba264c70bad3e58efc1a5f7a0da684cb811c
fana abera <fanaabera@gmail.com>
2022-08-07 09:24:30 -0700

    including additional prototypes

--- main.h
+++ main.h
@@ -10,5 +10,6 @@ int check_formater(char str, va_list *arg, int i, int *c);
 int _printf(const char *format, ...);
 int _puts(char *str, int i, int *c);
 int _putchar(int character, int i, int *c);
+int _putnumber(long n, int i, int *c);
 
 #endif /* MAIN_H */
